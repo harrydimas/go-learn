d995953633fad40ab85a5b6f1912383db97d4037
harrydimas <dimasharry07@gmail.com>
2023-09-22 16:56:00 +0700

    init push

---
+++ models/item_model.go
@@ -0,0 +1,8 @@
+ package models
+
+ import "github.com/jinzhu/gorm"
+
+ type Item struct {
+	gorm.Model
+	Name string `json:"name"`
+ }
---
+++ repos/item_repo.go
@@ -0,0 +1,31 @@
+package repos
+
+import (
+	"github.com/harrydimas/go-learn/models"
+	"github.com/jinzhu/gorm"
+)
+
+type ItemRepository struct{ DB *gorm.DB }
+
+func NewItemRepository(db *gorm.DB) *ItemRepository {
+	return &ItemRepository{DB: db}
+}
+func (r *ItemRepository) Create(item *models.Item) error {
+	return r.DB.Create(item).Error
+}
+func (r *ItemRepository) FindAll() ([]models.Item, error) {
+	var items []models.Item
+	err := r.DB.Find(&items).Error
+	return items, err
+}
+func (r *ItemRepository) FindByID(id uint) (*models.Item, error) {
+	var item models.Item
+	err := r.DB.First(&item, id).Error
+	return &item, err
+}
+func (r *ItemRepository) Update(item *models.Item) error {
+	return r.DB.Save(item).Error
+}
+func (r *ItemRepository) Delete(id uint) error {
+	return r.DB.Where("id = ?", id).Delete(models.Item{}).Error
+}
